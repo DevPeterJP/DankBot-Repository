f0939419adab503aac297edf4d370467e6b5cd4f
DevPeterJP <sportsmanpeter24@gmail.com>
2017-09-13 19:24:59 -0400

    Add files via upload

---
+++ leader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace DankBot.Modules
+{
+    public class leader : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("leader")]
+        public async Task Leadermsg()
+        {
+            await Context.Channel.SendMessageAsync("Calvin is the only leader, despite what Julian says or thinks. As a rule of thumb, discount what Julian says.");
+        }
+
+    }
+}
